667ec6fbf1c84d65bffaf0a56c78b02322290b53
John Patrick Salangsang <salangsangjpmoreno@gmail.com>
2025-04-02 23:48:04 +0800

    Updated flutter dependencies

---
+++ android/app/src/main/kotlin/com/example/numbertranslator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.numbertranslator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- numbertranslator/android/app/src/main/kotlin/com/example/numbertranslator/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.numbertranslator
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
